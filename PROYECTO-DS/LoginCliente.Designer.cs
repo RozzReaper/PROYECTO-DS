92371988ad09b2adb7e19c016139ecdba57a0497
José Leonardo Rodriguez Zelaya <leo1000player@gmail.com>
2022-03-03 17:57:13 -0600

    Cambios en la configuración de las propiedades del proyecto. Se sugiere realizar un pull para corroborar que a todos nos funcionen los cambios

--- PROYECTO-DS/LoginCliente.Designer.cs
+++ PROYECTO-DS/LoginCliente.Designer.cs
@@ -137,6 +137,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AntiqueWhite;
             this.ClientSize = new System.Drawing.Size(794, 510);
             this.Controls.Add(this.btn_regresar);
             this.Controls.Add(this.btnCerrar);
@@ -148,6 +149,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "LoginCliente";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LoginCliente";
